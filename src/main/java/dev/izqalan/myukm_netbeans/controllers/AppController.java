54cabd48a4e0173b7077465860d5ef8238fab624
izqalan <izqalan@gmail.com>
2022-06-13 23:23:37 +0800

    login boilerplate

---
+++ src/main/java/dev/izqalan/myukm_netbeans/controllers/AppController.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.izqalan.myukm_netbeans.controllers;
+
+import dev.izqalan.myukm_netbeans.models.UserModel;
+import dev.izqalan.myukm_netbeans.views.LoginView;
+import dev.izqalan.myukm_netbeans.views.MainMenu;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Izqalan, Peggy, Lam
+ */
+public class AppController {
+
+    UserModel user = new UserModel();
+    public static HashMap<String, UserModel> usersList = new HashMap<>();
+    
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        AppController.seed();
+        loginView.run();
+    }
+    
+    
+    public static void seed(){
+        String email = "admin";
+        String password = "admin";
+        UserModel permaUser = new UserModel(email, password);
+        usersList.put(email, permaUser);
+    }
+
+    /**
+     * @param email
+     * @param password
+     * @return true if login success else false
+     */
+    public boolean login(String email, String password) {
+        try{
+            if (usersList.containsKey(email) && usersList.get(email).getPassword().equals(password)){
+                user.setCurrentUser(new UserModel(email, password));
+            }
+        } catch (Error e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param email
+     * @param password
+     * @return true if registration successful else false
+     */
+    public boolean register(String email, String password) {
+        try {
+            user.setEmail(email);
+            user.setPassword(password);
+            // save user in local memory
+            usersList.put(email, user);
+        } catch (Error e){
+            return false;
+        }
+        return true;
+    }
+    
+    public void viewMainMenu(){
+        new MainMenu();
+    }
+}
---
+++ src/main/java/dev/izqalan/myukm_netbeans/models/UserModel.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.izqalan.myukm_netbeans.models;
+
+/**
+ *
+ * @author Izqalan
+ */
+public class UserModel {
+    
+    private String email;
+    private String password;
+    private UserModel currentUser;
+
+    public UserModel() {
+    }
+    
+    public UserModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserModel getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(UserModel currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
